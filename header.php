9a4bf05a1e73a49a3c275e0215358c636e667a50
Lucas <lucaslaurens.74@gmail.com>
2017-12-12 14:29:08 +0100

    Smooth_Scrolling
    les menus faudra les rajouter sur chacund e vos wp mais le smooth est bon

--- header.php
+++ header.php
@@ -6,8 +6,8 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/bootstrap.min.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/style.css">
-   <script type="text/javascript" src="<?php bloginfo('url'); ?>/wp-content/themes/TEDX/js/main.js"></script>
-  <script type="text/javascript" src="<?php bloginfo('url'); ?>/wp-content/themes/TEDX/smooth_scrolling.js"></script>
+  <script type="text/javascript" src="<?php bloginfo('url'); ?>/wp-content/themes/BapTedx/BapTedx/js/main.js"></script>
+ <script type="text/javascript" src="<?php bloginfo('url'); ?>/wp-content/themes/BapTedx/BapTedx/js/smooth_scrolling.js"></script>
   <meta charset="utf-8"/>
 
 
@@ -64,10 +64,3 @@
    </div>
 
 </nav>
-
-<div style="height:50vh;background-color:#0F0;"><h2 style="color: #fff; text-align: center; margin: 0 !important; padding: 5%;">Image head</h2></div>
-<div style="height:50vh;background-color:#00F;" id="test"><h2 style="color: #fff; text-align: center; margin: 0 !important; padding: 5%;">Billeterie</h2></div>
-<div style="height:50vh;background-color:#F00;"><h2 style="color: #fff; text-align: center; margin: 0 !important; padding: 5%;">Articles</h2></div>
-<div style="height:50vh;background-color:#0F0;"><h2 style="color: #fff; text-align: center; margin: 0 !important; padding: 5%;">Partenaires</h2></div>
-
-
--- index.php
+++ index.php
@@ -9,6 +9,14 @@
 
 	
 
+<div id="accueil" style="height:50vh;background-color:#0F0;"><h2 style="color: #fff; text-align: center; margin: 0 !important; padding: 5%;">Image head</h2></div>
+<div id="billeterie" style="height:50vh;background-color:#00F;" id="test"><h2 style="color: #fff; text-align: center; margin: 0 !important; padding: 5%;">Billeterie</h2></div>
+<div id="actualites" style="height:50vh;background-color:#F00;"><h2 style="color: #fff; text-align: center; margin: 0 !important; padding: 5%;">Actualités</h2></div>
+<div id="equipe" style="height:50vh;background-color:#0F0;"><h2 style="color: #fff; text-align: center; margin: 0 !important; padding: 5%;">Équipe</h2></div>
+<div id="partenaire" style="height:50vh;background-color:#00F;"><h2 style="color: #fff; text-align: center; margin: 0 !important; padding: 5%;">Partenaires</div>
+
+
+
 
 
 
